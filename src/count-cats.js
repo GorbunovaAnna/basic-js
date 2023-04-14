fc25528c72d70fbba59a0180101fbbeabc28a8c8
GorbunovaAnna <anyutachke@gmail.com>
2023-04-14 17:30:52 +0300

    feat: add solution dream-team and count-cats tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats( matrix ) {
+  let res = 0;
+
+ for(let i = 0; i < matrix.length; i++) {
+  for(let j = 0; j < matrix[i].length; j++) {
+    matrix[i][j] === '^^' ? res += 1 : res;
+  }
+ }
+ return res;
 }
+countCats([ [0, 1, '^^'], [0, '^^', 2], ['^^', 1, 2] ]); 
 
 module.exports = {
   countCats
